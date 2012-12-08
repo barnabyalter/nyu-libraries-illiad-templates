979ee00b86342621689c6397e5c73d498f61a7e2
barnabyalter <barnaby.alter@gmail.com>
2012-12-07 19:06:40 -0500

    Updated text

--- views/illiad_main_menu.rb
+++ views/illiad_main_menu.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'text_page')
 class IlliadMainMenu < TextPage
   
   def page_header
-    "NYU Interlibrary Loan (ILL) has a new look!"
+    "Interlibrary Loan (ILL)"
   end
   
   def text_sections
@@ -11,10 +11,8 @@ class IlliadMainMenu < TextPage
       :text_class => "text_section", 
       :text_id => "status",
       :paragraphs => [
-        "<p>Our interlibrary loan request system is now accessible directly from your e-Shelf. </p> ",
-        "<p>Use the links to the left to request materials that are unavailable in the New York University or consortium libraries and to view current and past requests. </p>",
-        "<p>More information on interlibrary loan can be found here: <a href=\"https://library.nyu.edu/services/ill.html\" target=\"_blank\">https://library.nyu.edu/services/ill.html</a></p>",
-        "<p>Questions? Contact Ask a Librarian: <a href=\"https://library.nyu.edu/ask/\" target=\"_blank\">https://library.nyu.edu/ask/</a></p>"
+        "<p>Use the links to the left to request materials that are unavailable in the New York University or consortium libraries and to view current and past requests.</p> ",
+        "<p>Want more information? Visit <a href=\"https://library.nyu.edu/services/ill.html\" target=\"_blank\">https://library.nyu.edu/services/ill.html</a> for details about who can use ILL, how long it takes, how to pick up materials, etc.</p>"
       ]
     }
     ]
