7732ca204ec97b752108dd91b7316d4aad6e0cc7
B Alter <barnaby.alter@gmail.com>
2013-09-06 16:14:44 -0400

    Updated pickup locations with Shanghai

--- views/book_chapter_request.rb
+++ views/book_chapter_request.rb
@@ -20,7 +20,7 @@ class BookChapterRequest < Form
         {:field => "ISSN", :field_title => "ISBN #{isbn_popup}", :note => "Will speed processing.", :input_text => true},
         {:field => "NotWantedAfter", :field_title => "I need this material by:", :required => true, :note => "Plan ahead when requesting materials, if the \"not wanted after date\" is sooner than 7 days than the request will be canceled.", :input_text => true},
         {:field => "AcceptNonEnglish", :field_title => "Accept chapter in languages other than English? ", :note => "If yes, specify languages in \"Notes\" below.", :select => {:options => [{:value => "No"},{:value => "Yes"}]}},
-        {:field => "ShippingOptions", :field_title => 'Are you an NYU student at a global site? Request a scan of an NYU item by indicating your location:', :note => "Request must be within <a href=\"http://nyu.libguides.com/content.php?pid=133679&sid=1146824\" target=\"_blank\">fair use guidleines</a>.", :select => {:options => [{:value => "Abu Dhabi"},{:value => "Other NYU Global Site"}]} },
+        {:field => "ShippingOptions", :field_title => 'Are you an NYU student at a global site? Request a scan of an NYU item by indicating your location:', :note => "Request must be within <a href=\"http://nyu.libguides.com/content.php?pid=133679&sid=1146824\" target=\"_blank\">fair use guidleines</a>.", :select => {:options => [{:value => "Abu Dhabi"},{:value => "Shanghai"},{:value => "Other NYU Global Site"}]} },
         {:field => "CitedIn", :field_title => 'Please specify "Other" global site', :input_text => true},
         {:field => "Notes", :field_title => 'Notes', :textarea => true}     
        ]
