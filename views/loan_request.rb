9796522dc9884e836ad3c34e07d3c8760467c65c
B Alter <barnaby.alter@gmail.com>
2014-05-23 15:36:41 -0400

    Added Poly and Courant to pickup locations

--- views/loan_request.rb
+++ views/loan_request.rb
@@ -3,12 +3,12 @@ class LoanRequest < Form
   def page_name
     "Request a Book"
   end
-  
-  def form 
+
+  def form
     super.merge!({
        :form_fields => [
         {:field => "LoanTitle", :field_title => "Title", :required => true, :note => "Please do not abbreviate unless your citation is abbreviated", :input_text => true},
-        {:field => "LoanAuthor", :field_title => "Author/Editors", :required => true, :input_text => true},         
+        {:field => "LoanAuthor", :field_title => "Author/Editors", :required => true, :input_text => true},
         {:field => "LoanPlace", :field_title => "Place of Publication", :input_text => true},
         {:field => "LoanPublisher", :field_title => "Publisher", :input_text => true},
         {:field => "LoanDate", :field_title => "Publication Date", :required => true, :input_text => true},
@@ -18,12 +18,12 @@ class LoanRequest < Form
         {:field => "NotWantedAfter", :field_title => "I need this material by:", :required => true, :note => "Plan ahead when requesting materials, if the \"not wanted after date\" is sooner than 7 days than the request will be canceled.", :input_text => true},
         {:field => "AcceptNonEnglish", :field_title => "Accept book in languages other than English?", :note => "If yes, specify languages in \"Notes\" below.", :select => {:options => [{:value => "No"},{:value => "Yes"}] }},
         {:field => "AcceptAlternateEdition", :field_title => "Accept an alternate edition?", :select => {:options => [{:value => "No"},{:value => "Yes"}] }},
-        {:field => "ShippingOptions", :field_title => "Pickup Location", :required => true, :select => {:options => [{:value => "NYU Bobst"},{:value => "NYU Abu Dhabi"},{:value => "NYU Shanghai"},{:value => "NYU Institute of Fine Arts"},{:value => "NYU Inst Study Ancient World"},{:value => "NYU Jack Brause"}]} },
-        {:field => "Notes", :field_title => 'Notes', :textarea => true}     
+        {:field => "ShippingOptions", :field_title => "Pickup Location", :required => true, :select => {:options => [{:value => "NYU Bobst"},{:value => "NYU Abu Dhabi"},{:value => "NYU Shanghai"},{:value => "NYU Bern Dibner"},{:value => "NYU Courant"},{:value => "NYU Institute of Fine Arts"},{:value => "NYU Inst Study Ancient World"},{:value => "NYU Jack Brause"}]} },
+        {:field => "Notes", :field_title => 'Notes', :textarea => true}
        ]
      })
    end
-   
+
    def pre_form_text
       '<div class="linkout nyulibrary_icons_information">
         Just need a chapter? Use the <a href="'+action_tag({:action=>"10",:form=>"23"})+'">Book Chapter</a> form.<br />
