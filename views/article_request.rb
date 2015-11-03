6fc5bc96d5e11ee52b998f9d04a2e58c60228aba
B Alter <barnaby.alter@gmail.com>
2015-11-03 12:37:53 -0500

    Modify article form to test deploy

--- views/article_request.rb
+++ views/article_request.rb
@@ -1,14 +1,14 @@
 require File.join(File.dirname(__FILE__), 'form')
 class ArticleRequest < Form
-  
+
   def page_name
     "Request an Article"
   end
-  
-  def form 
+
+  def form
      super.merge!({
        :form_fields => [
-        {:field => "PhotoArticleTitle", :field_title => "Article Title", :required => true, :input_text => true},
+        {:field => "PhotoArticleTitle", :field_title => "Article Title (1)", :required => true, :input_text => true},
         {:field => "PhotoArticleAuthor", :field_title => "Article Author", :input_text => true},
         {:field => "PhotoJournalTitle", :field_title => "Journal Title", :required => true, :note => "Please do not abbreviate unless your citation is abbreviated", :input_text => true},
         {:field => "PhotoJournalVolume", :field_title => "Volume Number", :required => true, :input_text => true},
@@ -21,11 +21,11 @@ class ArticleRequest < Form
         {:field => "AcceptNonEnglish", :field_title => "Accept article in languages other than English? ", :note => "If yes, specify languages in \"Notes\" below.", :select => {:options => [{:value => "No"},{:value => "Yes"}] }},
         {:field => "ShippingOptions", :field_title => 'Are you an NYU student at a global site? Request a scan of an NYU item by indicating your location:', :note => "Request must be within <a href=\"http://nyu.libguides.com/content.php?pid=133679&sid=1146824\" target=\"_blank\">fair use guidleines</a>.", :select => {:options => [{:value => "Abu Dhabi", :title => "Abu Dhabi"},{:value => "Shanghai", :title => "Shanghai"},{:value => "Other NYU Global Site", :title => "Other NYU Global Site"}]} },
         {:field => "CitedIn", :field_title => 'Please specify "Other" global site', :input_text => true},
-        {:field => "Notes", :field_title => 'Notes', :textarea => true}     
+        {:field => "Notes", :field_title => 'Notes', :textarea => true}
        ]
      })
    end
-   
+
    def pre_form_text
      '<div class="linkout nyulibrary_icons_information">Need entire volume(s) or issue(s)? Use the <a href="'+action_tag({:action=>"10",:form=>"20",:value=>"GenericRequestJournal"})+'">Journal</a> form.</div>'
    end
