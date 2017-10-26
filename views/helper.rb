2bd88052625a887854d45a32e19fa1d59606facb
Barnaby Alter <barnaby.alter@nyu.edu>
2017-10-26 13:09:54 -0400

    Make icon links go to blank target

--- views/helper.rb
+++ views/helper.rb
@@ -7,36 +7,36 @@ module Helper
     str += ">"
     return str
   end
-  
+
   def action_tag params = {}
     app_tag "ACTION", params
   end
-  
+
   def status
     app_tag "STATUS"
   end
-  
+
   def footer
     include_tag "include_footer.html"
   end
-  
+
   def include_tag filename
     app_tag "INCLUDE", { :filename => filename }
   end
-  
+
   def menu_tag params = { :name => "transaction", :separator => "|" }
     app_tag "MENU", params
   end
-  
+
   def param_tag name
     app_tag "PARAM", { :name => name }
   end
-  
+
   def error_tag name
     app_tag "ERROR", { :name => "ERROR#{name}" }
   end
-  
-  def icon_link url, css_classes = ["nyulibrary_icons_help", "iconlink"], title = nil
-    "<a href=\"#{url}\" class=\"#{css_classes.join(" ")}\" title=\"#{title}\">&nbsp;</a>"
+
+  def icon_link url, target="_blank", css_classes = ["nyulibrary_icons_help", "iconlink"], title = nil
+    "<a href=\"#{url}\" class=\"#{css_classes.join(" ")}\" title=\"#{title}\" target=\"#{target}\">&nbsp;</a>"
   end
 end
