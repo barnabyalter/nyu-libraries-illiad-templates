be7954716d6d961c13a259ecb316952d941ddd14
Barnaby Alter <barnaby.alter@nyu.edu>
2017-12-14 16:05:01 -0500

    Add tables to homepage and remove from the sidebar

--- views/illiad_main_menu.rb
+++ views/illiad_main_menu.rb
@@ -1,19 +1,34 @@
-require File.join(File.dirname(__FILE__), 'text_page')
-class IlliadMainMenu < TextPage
+require File.join(File.dirname(__FILE__), 'table')
+class IlliadMainMenu < Table
 
   def page_header
     "Interlibrary Loan (ILL)"
   end
 
-  def text_sections
+  def tables
     [
     {
-      :text_class => "text_section",
-      :text_id => "status",
-      :paragraphs => [
-        "<p>Use the links to the left to request materials that are unavailable in the New York University or consortium libraries and to view current and past requests.</p> ",
-        "<p>Want more information? Visit <a href=\"https://library.nyu.edu/services/borrowing/from-non-nyu-libraries/interlibrary-loan/\" target=\"_blank\">https://library.nyu.edu/services/borrowing/from-non-nyu-libraries/interlibrary-loan/</a> for details about who can use ILL, how long it takes, how to pick up materials, etc.</p>"
-      ]
+      :class => "default-table",
+      :name => "ViewOutstandingRequests",
+      :headerText => "Requests in Processing",
+      :noDataAction => "ShowMessageRow",
+      :noDataMessage => "No Requests",
+      :columns => ["TransactionNumber:Transaction Number (TN)","DocumentType:Format Type","Title","Author","CreationDate:Request Submitted"],
+      :orderBy => "TransactionNumber ASC"
+    },
+    {
+      :class => "default-table",
+      :name => "ElectronicDelivery",
+      :headerText => "Documents Available to Save/Print",
+      :noDataAction => "ShowMessageRow",
+      :noDataMessage => "No Articles",
+      :columns => ["TransactionNumber:Transaction Number (TN)","View:Click here to view your document","DocumentType:Format Type","Size","PhotoArticleTitle:Article/Chapter Title","PhotoArticleAuthor:Author","PhotoJournalTitle:Journal/Book Title","Expires","Delete"],
+      :orderBy => "TransactionNumber DESC",
+      :text_above => '
+        <p>Articles posted to this page will remain accessible for <strong>15 days from the date of posting or for a maximum of 5 views</strong>. You are encouraged to print or save the document for your future use.</p>
+        <p>If, prior to this time, you no longer require the use of an article, you may delete it using the "Delete" link to the right of the article information. Deleted articles cannot be recovered or re-requested.</p>
+        <p>If you have trouble viewing your document, please contact the Interlibrary Loan Office at 212-998-2511 or <a href="mailto:ill@library.nyu.edu">ill@library.nyu.edu</a></p>
+      '
     }
     ]
   end
