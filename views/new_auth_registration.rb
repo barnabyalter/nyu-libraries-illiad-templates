bc5b01e0704e028cbd43863c29740ba279b07253
BAlter <barnaby.alter@nyu.edu>
2019-02-06 15:57:28 -0500

    Remove testing

--- views/new_auth_registration.rb
+++ views/new_auth_registration.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'text_page')
 class NewAuthRegistration < TextPage
 
   def page_header
-    "TESTING - Welcome to NYU Interlibrary Loan Request System"
+    "Welcome to NYU Interlibrary Loan Request System"
   end
 
   def page_name
