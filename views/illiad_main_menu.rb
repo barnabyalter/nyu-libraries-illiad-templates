ca41f0e7a1a8e9c97198d8d8ef35fbab2b83eb75
BAlter <barnaby.alter@nyu.edu>
2019-02-19 14:26:28 -0500

    Remove placeholder text to test CICD

--- views/illiad_main_menu.rb
+++ views/illiad_main_menu.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'table')
 class IlliadMainMenu < Table
 
   def page_header
-    "TEST- Interlibrary Loan (ILL)"
+    "Interlibrary Loan (ILL)"
   end
 
   def tables
