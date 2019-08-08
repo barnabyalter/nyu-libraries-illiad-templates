c717ac33b1dbdca57c4be5b06630c458a4eb9196
BAlter <barnaby.alter@nyu.edu>
2019-08-08 10:39:40 -0400

    Remove gauges

--- views/include_footer.rb
+++ views/include_footer.rb
@@ -5,23 +5,6 @@ class IncludeFooter < Mustache
   def footer
     app_tag "COPYRIGHT"
   end
-  
-  def gauges
-    "
-    <script type=\"text/javascript\">
-      var _gauges = _gauges || [];
-      (function() {
-        var t   = document.createElement('script');
-        t.type  = 'text/javascript';
-        t.async = true;
-        t.id    = 'gauges-tracker';
-        t.setAttribute('data-site-id', '5141cd8bf5a1f56376000050');
-        t.src = '//secure.gaug.es/track.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(t, s);
-      })();
-    </script>
-    "
-  end
+
 
 end
