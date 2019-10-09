c8b92392fe7e449f5f61aa26bb3b1ea801f0403e
BAlter <barnaby.alter@nyu.edu>
2019-10-09 16:50:48 -0400

    Remove missing assets to avoid 404s

--- views/include_stylesheets.rb
+++ views/include_stylesheets.rb
@@ -11,8 +11,6 @@ class IncludeStylesheets < Mustache
   def stylesheets
     [
       {:type => @type, :rel => @rel, :href => "#{@folder}/illiad.css", :media => "screen,projector"},
-      {:type => @type, :rel => @rel, :href => "#{@folder}/print.css", :media => "print"},
-      {:ie => true, :type => @type, :rel => @rel, :href => "#{@folder}/ie.css", :media => "screen,projector"}
     ]
   end
 
