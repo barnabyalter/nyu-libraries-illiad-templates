f8bb4d975e89dc93f4631fd488ba1db48fcc788c
BAlter <barnaby.alter@nyu.edu>
2020-07-28 11:57:58 -0400

    Add link to change information form to sidebar

--- views/include_menu.rb
+++ views/include_menu.rb
@@ -12,7 +12,8 @@ class IncludeMenu < Mustache
             [
               { :link => "https://eshelf.library.nyu.edu/account" , :id => "account", :title => "My library account" },
               { :link => "/" , :id => "ill", :title => "My interlibrary loan (ILL)", :class => "selected" },
-              { :link => "https://eshelf.library.nyu.edu" , :id => "records", :title => "All saved records" }
+              { :link => "https://eshelf.library.nyu.edu" , :id => "records", :title => "All saved records" },
+              { :link => action_tag({:action=>"10",:form=>"81"}), :id => "change_user_information", :title => "Update My Address" }
             ]
           },
           { :id => "request", :header => "Make a request", :options =>
