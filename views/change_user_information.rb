e9351385dd567402a52ec64f3a1d2958ff740806
BAlter <barnaby.alter@nyu.edu>
2020-07-29 15:44:35 -0400

    Remove default select value for state

--- views/change_user_information.rb
+++ views/change_user_information.rb
@@ -19,12 +19,12 @@ class ChangeUserInformation < Form
         {field: "Address", value: param_tag("Address"), field_title: "Primary Address Line 1", required: true, input_text: true},
         {field: "Address2", value: param_tag("Address2"), field_title: "Primary Address Line 2", input_text: true},
         {field: "City", value: param_tag("City"), field_title: "Primary Address City", required: true, input_text: true},
-        {field: "State", value: param_tag("State"), field_title: "Primary Address State", required: true, select: true, options_group: '<#OPTION name="custom" groupname="State" selectedValue="<#PARAM name=State>" defaultValue="NONE" defaultName="Choose your State">'},
+        {field: "State", value: param_tag("State"), field_title: "Primary Address State", required: true, select: true, options_group: '<#OPTION name="custom" groupname="State" selectedValue="<#PARAM name=State>" defaultValue="NONE" defaultName="">'},
         {field: "Zip", value: param_tag("Zip"), field_title: "Primary Address Zip", required: true, input_text: true},
         {field: "SAddress", value: param_tag("SAddress"), field_title: "Secondary Address Line 1", input_text: true},
         {field: "SAddress2", value: param_tag("SAddress2"), field_title: "Secondary Address Line 2", input_text: true},
         {field: "SCity", value: param_tag("SCity"), field_title: "Secondary Address City", input_text: true},
-        {field: "SState", value: param_tag("SState"), field_title: "Secondary Address State", select: true, options_group: '<#OPTION name="custom" groupname="State" selectedValue="<#PARAM name=SState>" defaultValue="NONE" defaultName="Choose your State">'},
+        {field: "SState", value: param_tag("SState"), field_title: "Secondary Address State", select: true, options_group: '<#OPTION name="custom" groupname="State" selectedValue="<#PARAM name=SState>" defaultValue="NONE" defaultName="">'},
         {field: "SZip", value: param_tag("SZip"), field_title: "Secondary Address Zip", input_text: true},
       ].map {|field| field.merge(Hash[form_fields_maxlength.find{|f| f[:field] == field[:field]}.to_a]) }
      })
