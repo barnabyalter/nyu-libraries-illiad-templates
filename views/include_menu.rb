955eb97555a0cc6efeb9a1c4100296a5f027e8bf
BAlter <barnaby.alter@nyu.edu>
2020-08-13 18:02:43 -0400

    Updated sidebar menu options

--- views/include_menu.rb
+++ views/include_menu.rb
@@ -11,12 +11,12 @@ class IncludeMenu < Mustache
           { :id => "shelf_options", :header => nil, :options =>
             [
               { :link => "https://eshelf.library.nyu.edu/account" , :id => "account", :title => "My library account" },
-              { :link => "/" , :id => "ill", :title => "My interlibrary loan (ILL)", :class => "selected" },
+              { :link => "/" , :id => "ill", :title => "My Requests", :class => "selected" },
               { :link => "https://eshelf.library.nyu.edu" , :id => "records", :title => "All saved records" },
-              { :link => action_tag({:action=>"10",:form=>"81"}), :id => "change_user_information", :title => "Update my profile" }
+              { :link => action_tag({:action=>"10",:form=>"81"}), :id => "change_user_information", :title => "Update my delivery address" }
             ]
           },
-          { :id => "request", :header => "Make a request", :options =>
+          { :id => "request", :header => "Make an Interlibrary Loan (ILL) request", :options =>
             [
               { :link => action_tag({:action=>"10",:form=>"22"}), :id => "request_article", :title => "Article" },
               { :link => action_tag({:action=>"10",:form=>"21"}), :id => "request_book", :title => "Book" },
@@ -29,7 +29,7 @@ class IncludeMenu < Mustache
               { :link => action_tag({:action=>"10",:form=>"60"}) , :id => "view_all", :title => "Request history" }
             ]
           },
-          { :id => "search_illiad", :header => "Search ILL Requests", :content =>
+          { :id => "search_illiad", :header => "Search My Requests", :content =>
             '
             <form action="illiad.dll" name="Search" method="post" id="Search">
               <input type="hidden" name="IlliadForm" value="Search">
@@ -44,8 +44,9 @@ class IncludeMenu < Mustache
             </form>
             '
           },
-          { :id => "links", :header => "Additional Options", :options =>
+          { :id => "links", :header => "Service Information and Policies", :options =>
             [
+              { :link => "#", :target => "_blank" , :id => "delivery_policies", :title => "Delivery Services Info & Policies" },
               { :link => "https://library.nyu.edu/services/borrowing/from-non-nyu-libraries/interlibrary-loan/", :target => "_blank" , :id => "ill_policies", :title => "Interlibrary Loan (ILL) info & Policies" }
             ]
           }
